b160b71fdea566ce65f06e09256fc6b2b6243aa6
rabewarmor <145297327+rabewarmor@users.noreply.github.com>
2024-03-25 19:47:41 -0600

    Update 2DMotion.h

--- assignments/a3/q2/2DMotion.h
+++ assignments/a3/q2/2DMotion.h
@@ -1,26 +1,87 @@
-#ifndef MOTION2D
-#define MOTION2D
-#include <iostream>
 
-// Predefined constant for pi, to be used if/when converting degrees
-// to radians.
-#define PI 3.14159265
+Rabewar Moradi - 1702601
 
-// Forward declation of Force. Needed in case any of Point references
-// the existence of type Force.
-class Force;
+	Collaborated with:
+		- Syed Hasan Tamid - 1779230
+		- Gavin Vickers - 1838216
 
-class Point {
-    // Private fields and helpers here.
-  public:
-    // Required public interface here.
 
-};
+#include "2DMotion.h"
 
-class Force {
-    // Private fields and helpers here.
-  public:
-    // Required public interface here.
-};
+Point::Point()
+    : m_x(0), m_y(0)
+{}
 
-#endif
+Point::Point(float x, float y)
+    : m_x(x), m_y(y)
+{}
+
+Point::Point(const Point& p)
+    : m_x(p.m_x), m_y(p.m_y)
+{
+
+}
+
+std::istream& operator>>(std::istream& is, Point& point) {
+    is >> point.m_x;
+    is >> point.m_y;
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, Point& point) {
+    os << "(" << point.m_x << ", " << point.m_y << ")";
+    return os;
+}
+
+Point& operator+(Point& p, Force& f) {
+    int f_x = cos(f.getAngle()) * f.getMag();
+    int f_y = sin(f.getAngle()) * f.getMag();
+
+    p.m_x += f_x;
+    p.m_y += f_y;
+
+    Point new_point = Point(p);
+
+    return new_point;
+}
+
+int Point::quadrant() {
+    float angle = atan(m_y / m_x);
+    int quadrant = (angle / 90) + 1;
+
+    return quadrant;
+}
+
+// Force
+
+Force::Force()
+    : m_mag(0), m_angle(0)
+{}
+
+Force::Force(float mag, float angle)
+    : m_mag(mag), m_angle(angle)
+{}
+
+std::istream& operator>>(std::istream& is, Force& force) {
+    is >> force.m_mag;
+    is >> force.m_angle;
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, Force& force) {
+    os << force.m_angle << " degrees with magnitude of " << force.m_mag;
+    return os;
+}
+
+Force& operator*(Force& f, float& scalar) {
+    f.m_mag *= scalar;
+    return f;
+}
+
+float Force::getMag() {
+    return m_mag;
+}
+
+float Force::getAngle() {
+    return m_angle;
+}
